d626e4261ca0aa0ce9d4353015bc645c2bb71f4e
Java <Java@192.168.0.49>
2024-02-13 13:27:30 +0100

    Insercion de usuario en el app

--- TwiterHibernate/src/main/java/com/twiter/App.java
+++ TwiterHibernate/src/main/java/com/twiter/App.java
@@ -1,21 +1,52 @@
 package com.twiter;
 
+import com.twiter.accesodatos.RolAccesoDatos;
+import com.twiter.accesodatos.UsuarioAccesoDatos;
+import com.twiter.entidades.Rol;
+import com.twiter.entidades.Usuario;
+
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 
 public class App {
 
 	public static void main(String[] args) {
+		// roles para que se puedan crear usuarios
+		RolAccesoDatos.insertar(new Rol(null, "rolAdmin"));
+		
+		// obtener el rol para poder usarlo al crear el usuario
+		Rol rolAdmin = RolAccesoDatos.obtenerPorId(1);
+		
+		//registro (insert usuario)
+		UsuarioAccesoDatos.insertar(new Usuario("gonzalo", "contra", rolAdmin));
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void mainAntiguo(String[] args) {
 		// Iniciar la sesión de Hibernate
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("TwiterApp");
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("TwiterApp");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction t = em.getTransaction();
         
-        
+        t.begin();
         
         // Cerrar la sesión de Hibernate
-        entityManager.close();
-        entityManagerFactory.close();
+        em.close();
+        emf.close();
 	}
 
 }
