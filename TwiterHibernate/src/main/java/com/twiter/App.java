6b01c6aba6fb65afc046dfa74d568e34b1ad04ff
Java <Java@192.168.0.84>
2024-02-15 13:57:04 +0100

    ver seguidores y seguidos

--- TwiterHibernate/src/main/java/com/twiter/App.java
+++ TwiterHibernate/src/main/java/com/twiter/App.java
@@ -18,29 +18,33 @@ public class App {
 		// roles para que se puedan crear usuarios
 		RolAccesoDatos.insertar(new Rol(null, "ADMIN"));
 		RolAccesoDatos.insertar(new Rol(null, "USER"));
-		
+
 		// obtener el rol para poder usarlo al crear el usuario
 		Rol rolAdmin = RolAccesoDatos.obtenerPorId(1);
 		Rol rolUser = RolAccesoDatos.obtenerPorId(2);
-		
-		//registro (insert usuario)
+
+		// registro (insert usuario)
 		UsuarioAccesoDatos.insertar(new Usuario("gonzalo", "contra", rolAdmin));
 		UsuarioAccesoDatos.insertar(new Usuario("pepe", "pepe", rolUser));
 		UsuarioAccesoDatos.insertar(new Usuario("juan", "juan", rolUser));
-		
+
 		// loggeo (buscar el usuario)
 		Usuario gonzalo = UsuarioAccesoDatos.buscarPorNickName("gonzalo");
 		Usuario pepe = UsuarioAccesoDatos.buscarPorNickName("pepe");
-		
+		Usuario juan = UsuarioAccesoDatos.buscarPorNickName("juan");
+
 		// gestion de posts
 		Post primerPost = new Post(gonzalo, "Primer post de mi aplicacion de twiter");
 		PostAccesoDatos.insert(primerPost);
 		var posts = PostAccesoDatos.obtenerTodos();
 		var postsGonzalo = PostAccesoDatos.obtenerPorIdUsuario(gonzalo.getId());
-		
+
 		// seguimientos
 		UsuarioAccesoDatos.agregarSeguidor(pepe.getId(), gonzalo.getId());
-		
+		UsuarioAccesoDatos.agregarSeguidor(juan.getId(), gonzalo.getId());
+		var seguidoresGonzalo = UsuarioAccesoDatos.verSeguidores(gonzalo.getId());
+		var segidosPepe = UsuarioAccesoDatos.verSeguidos(pepe.getId());
+
 		// Salidas de consola ------------------------------------------------
 		// Ver usuarios
 		System.out.println("\nVer usuario gonzalo");
@@ -50,35 +54,25 @@ public class App {
 		System.out.println(posts);
 		System.out.println("\nVer Posts gonzalo");
 		System.out.println(postsGonzalo);
-		// Ver las personas a las que sigue gonzalo
-		
-		
+		// Ver seguidos/seguidores
+		System.out.println("\nVer seguidores gonzalo");
+		System.out.println(seguidoresGonzalo);
+		System.out.println("\nVer seguidos de pepe");
+		System.out.println(segidosPepe);
+
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 	public static void mainAntiguo(String[] args) {
 		// Iniciar la sesión de Hibernate
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("TwiterApp");
-        EntityManager em = emf.createEntityManager();
-        EntityTransaction t = em.getTransaction();
-        
-        t.begin();
-        
-        // Cerrar la sesión de Hibernate
-        em.close();
-        emf.close();
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("TwiterApp");
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction t = em.getTransaction();
+
+		t.begin();
+
+		// Cerrar la sesión de Hibernate
+		em.close();
+		emf.close();
 	}
 
 }
--- TwiterHibernate/src/main/java/com/twiter/accesodatos/PostAccesoDatos.java
+++ TwiterHibernate/src/main/java/com/twiter/accesodatos/PostAccesoDatos.java
@@ -24,7 +24,7 @@ public class PostAccesoDatos {
 	public static List<Post> obtenerPorIdUsuario(long id) {
 		System.err.println("\n Obtener posts por usuario id");
 		return enTransaccion(em -> em.createQuery(
-				"select post from Post as post join fetch post.usuario where post.usuario.id = :id order by post.fecha desc",
+				"select post from Post as post join fetch post.usuario join fetch usuario.rol where post.usuario.id = :id order by post.fecha desc",
 				Post.class).setParameter("id", id).getResultList());
 	}
 }
